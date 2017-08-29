400ef70623eeed21fba6b38b47c7c0cd0e6c5942
chandran <ackpc91@gmail.com>
2017-08-29 12:01:18 -0700

    Drew circle.

--- app/src/main/java/com/qburst/akhil/pookkalam/CanvasView.java
+++ app/src/main/java/com/qburst/akhil/pookkalam/CanvasView.java
@@ -1,7 +1,12 @@
 package com.qburst.akhil.pookkalam;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.widget.ImageView;
 
 /**
@@ -10,7 +15,29 @@ import android.widget.ImageView;
 
 public class CanvasView extends ImageView {
     Paint paint;
+    Context context;
+    Point center;
+    int widthHeight[];
+
     public CanvasView(Context context) {
         super(context);
+        center = new Point();
+        this.context = context;
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.BLACK);
+        //paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.));
+        paint.setStrokeWidth(5);
+        paint.setStyle(Paint.Style.STROKE);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawCircle(center.x, center.y, center.x - 20, paint);
+    }
+
+    public void setWidthHeight(int[] widthHeight) {
+        this.widthHeight = widthHeight;
+        center.set(widthHeight[0] / 2, widthHeight[1] / 2);
     }
 }
--- app/src/main/java/com/qburst/akhil/pookkalam/MainActivity.java
+++ app/src/main/java/com/qburst/akhil/pookkalam/MainActivity.java
@@ -1,13 +1,37 @@
 package com.qburst.akhil.pookkalam;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+    CanvasView canvasView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        canvasView = new CanvasView(this);
+        canvasView.setWidthHeight(getScreenResolution(this));
+        setContentView(canvasView);
+        canvasView.invalidate();
     }
+
+
+    private static int[] getScreenResolution(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
+        int widthHeight[] = new int[2];
+        widthHeight[0] = width;
+        widthHeight[1] = height;
+        return widthHeight;
+    }
+
+
 }
