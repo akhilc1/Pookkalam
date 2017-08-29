0c9b6fb082767da24d7dda19431146e16e9e5dd0
chandran <ackpc91@gmail.com>
2017-08-29 11:09:42 -0700

    First Commit.

---
+++ app/src/main/java/com/qburst/akhil/pookkalam/CanvasView.java
@@ -0,0 +1,16 @@
+package com.qburst.akhil.pookkalam;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.widget.ImageView;
+
+/**
+ * Created by guru on 29-08-17.
+ */
+
+public class CanvasView extends ImageView {
+    Paint paint;
+    public CanvasView(Context context) {
+        super(context);
+    }
+}
