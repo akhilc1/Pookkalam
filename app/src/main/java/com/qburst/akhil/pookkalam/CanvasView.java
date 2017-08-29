75d1d896641e5a3212052acd4fa46ff70022617f
chandran <ackpc91@gmail.com>
2017-08-29 13:13:36 -0700

    Triangle drawing complete.

--- app/src/main/java/com/qburst/akhil/pookkalam/CanvasView.java
+++ app/src/main/java/com/qburst/akhil/pookkalam/CanvasView.java
@@ -37,18 +37,32 @@ public class CanvasView extends ImageView {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        drawInnerCircle(canvas);
+        //drawInnerCircle(canvas);
         drawOuterCircle(canvas);
     }
 
     private void drawInnerCircle(Canvas canvas) {
+        paint.setColor(Color.GREEN);
         canvas.drawCircle(center.x, center.y, radius / 2, paint);
-        drawCircumscribedTriangle(canvas, center.x, center.y, radius/2, paint);
+        drawCircumscribedTriangle(canvas, center.x, center.y, radius / 2, paint);
     }
 
     private void drawOuterCircle(Canvas canvas) {
         canvas.drawCircle(center.x, center.y, radius, paint);
         drawCircumscribedTriangle(canvas, center.x, center.y, radius, paint);
+        drawInvertedTriangle(canvas, center.x, center.y, radius, paint);
+    }
+
+    private void drawInvertedTriangle(Canvas canvas, int x, int y, int radius, Paint paint) {
+        float xOffsetFromCenter = (float) (Math.cos((float) Math.PI / 6) * radius);
+        float yOffsetFromCenter = (float) (Math.sin((float) Math.PI / 6) * radius);
+        paint.setColor(Color.RED);
+        canvas.drawLine(x, y + radius, x - xOffsetFromCenter, y - yOffsetFromCenter, paint);
+        paint.setColor(Color.BLUE);
+        canvas.drawLine(x - xOffsetFromCenter, y - yOffsetFromCenter, x + xOffsetFromCenter,
+                y - yOffsetFromCenter, paint);
+        paint.setColor(Color.GREEN);
+        canvas.drawLine(x + xOffsetFromCenter,y-yOffsetFromCenter, x, y + radius, paint);
     }
 
     public void setWidthHeight(int[] widthHeight) {
@@ -61,10 +75,12 @@ public class CanvasView extends ImageView {
     private void drawCircumscribedTriangle(Canvas canvas, float circleCenterX, float circleCenterY, int radius, Paint paint) {
         float xOffsetFromCenter = (float) (Math.cos((float) Math.PI / 6) * radius);
         float yOffsetFromCenter = (float) (Math.sin((float) Math.PI / 6) * radius);
-
+        paint.setColor(Color.RED);
         canvas.drawLine(circleCenterX, circleCenterY - radius, circleCenterX + xOffsetFromCenter, circleCenterY + yOffsetFromCenter, paint);
+        paint.setColor(Color.BLUE);
         canvas.drawLine(circleCenterX + xOffsetFromCenter, circleCenterY + yOffsetFromCenter, circleCenterX - xOffsetFromCenter,
                 circleCenterY + yOffsetFromCenter, paint);
+        paint.setColor(Color.GREEN);
         canvas.drawLine(circleCenterX - xOffsetFromCenter, circleCenterY + yOffsetFromCenter, circleCenterX, circleCenterY - radius, paint);
     }
 }
